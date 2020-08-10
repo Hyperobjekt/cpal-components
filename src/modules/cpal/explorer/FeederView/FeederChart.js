18bf1b78f936f5a2689b7ba10ff014a48f15ed7d
James Minton <james-minton@github.com>
2020-08-09 20:55:38 -0500

    Update FeederChart.js

--- src/modules/cpal/explorer/FeederView/FeederChart.js
+++ src/modules/cpal/explorer/FeederView/FeederChart.js
@@ -139,7 +139,10 @@ const FeederChart = ({ ...props }) => {
         'UI_FEEDER_FEEDER_CHART_DESC',
       )}
     >
-      <h2>Dallas ISD School Feeder Patterns</h2>
+      <div className="feeder-chart-header">
+        <h2>Dallas ISD School Feeder Patterns</h2>
+      </div>
+       
       {buildFeederData().map(el => {
         // to manage tooltip state
         const [tooltipOpen, setTooltipOpen] = useState(
