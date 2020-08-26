1751710a5fd919bc9dff27f8bcc0818aea932338
A Groshek <amy@amygroshek.com>
2020-08-26 13:09:55 -0500

    Updates to a single color scale, remove other category color sets

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -10,19 +10,6 @@ export const SELECTED_COLORS = [
   '#FF0C0C',
 ]
 
-/**
- * colors for map choropleths
- */
-export const CHOROPLETH_COLORS = [
-  '#174b80',
-  '#4189d2',
-  '#abd9e9',
-  '#f7f7f7',
-  '#bfe9ab',
-  '#2fb57f',
-  '#136e4a',
-]
-
 // Colors for school zone (2mi radius)
 export const SCHOOL_ZONE_COLORS = {
   outline: 'rgba(0, 0, 0, 0.2)',
@@ -53,96 +40,11 @@ export const DEM_COLORS = [
  * colors for CRI scale
  */
 export const CRI_COLORS = [
-  '#602FEE',
-  '#7B53EF',
-  '#9979F8',
-  '#AF96F6',
-  '#CABBF5',
-  // '#3800D9',
-  // '#5225d3',
-  // '#795dc9',
-  // '#9e90c6',
-  // '#b6b0c6',
-]
-
-/**
- * colors for economics scale
- */
-export const ECON_COLORS = [
-  '#1B7D72',
-  '#31AB9D',
-  '#64CBBF',
-  '#8EE1D7',
-  '#C3EFEA',
-  // '#0225DF',
-  // '#2038B5',
-  // '#465CD2',
-  // '#6076E9',
-  // '#8397FE',
-]
-
-/**
- * colors for education scale
- */
-export const EDU_COLORS = [
-  '#B02180',
-  '#CF3D9E',
-  '#E459B5',
-  '#EE79C6',
-  '#FFA5E0',
-  // '#7C0000',
-  // '#922020',
-  // '#BA4C4C',
-  // '#DF6C6C',
-  // '#FC8B8B',
-]
-
-/**
- * colors for family scale
- */
-export const FAM_COLORS = [
-  '#255171',
-  '#3474A1',
-  '#549CCF',
-  '#6BB6EB',
-  '#95D3FF',
-  // '#AB5700',
-  // '#C27627',
-  // '#D7934D',
-  // '#E8AA69',
-  // '#FEC387',
-]
-
-/**
- * colors for health scale
- */
-export const HEL_COLORS = [
-  '#C84320',
-  '#D75E3E',
-  '#E27C61',
-  '#F19A83',
-  '#FFBEAD',
-  // '#008511',
-  // '#1C992C',
-  // '#47B555',
-  // '#74DD81',
-  // '#9BFDA7',
-]
-
-/**
- * colors for community scale
- */
-export const COMM_COLORS = [
-  '#CDB314',
-  '#E3C81E',
-  '#F1D734',
-  '#FBE350',
-  '#FFEC75',
-  // '#AA00A5',
-  // '#BB20B6',
-  // '#D041CB',
-  // '#E660E2',
-  // '#FE84FA',
+  '#465EB3',
+  '#80A2D3',
+  '#C1EFF6',
+  '#85D4B6',
+  '#51BC7E',
 ]
 
 export const DISTRICT_COLORS = {
